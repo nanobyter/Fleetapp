b6b8a9c6a0159198016c6a86ac3ef1bcc819538f
nanobyter <sotarfredrik@gmail.com>
2022-09-07 22:12:05 +0200

    Created Controllers

---
+++ src/main/java/tech/nanobyter/fleetapp/controllers/VehicleMakeController.java
@@ -0,0 +1,14 @@
+package tech.nanobyter.fleetapp.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author nanobyter	
+ * Created: 2022-09-07
+ *
+ */
+
+@Controller
+public class VehicleMakeController {
+
+}
