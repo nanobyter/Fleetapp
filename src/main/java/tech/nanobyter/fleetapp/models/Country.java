fc25810bc3c8acb10e4c9bb395c128d0e485d848
nanobyter <sotarfredrik@gmail.com>
2022-09-09 13:30:13 +0200

    Fixed bug in Lobok (Country.java model) not using getters and setters

--- src/main/java/tech/nanobyter/fleetapp/models/Country.java
+++ src/main/java/tech/nanobyter/fleetapp/models/Country.java
@@ -8,19 +8,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * @author nanobyter Created: 2022-09-07
  *
  */
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Country {
 
 	@Id
@@ -34,5 +27,85 @@ public class Country {
 
 	@OneToMany(mappedBy = "country")
 	private List<State> states;
+
+	public Country(Integer id, String code, String capital, String description, String nationality, String continent,
+			List<State> states) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.capital = capital;
+		this.description = description;
+		this.nationality = nationality;
+		this.continent = continent;
+		this.states = states;
+	}
+
+	public Country() {
+		super();
+	}
+
+	public Country(Integer id, String code, String capital, String description) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.capital = capital;
+		this.description = description;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getCapital() {
+		return capital;
+	}
+
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public String getContinent() {
+		return continent;
+	}
+
+	public void setContinent(String continent) {
+		this.continent = continent;
+	}
+
+	public List<State> getStates() {
+		return states;
+	}
+
+	public void setStates(List<State> states) {
+		this.states = states;
+	}
 
 }
